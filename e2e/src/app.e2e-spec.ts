eac2693b08b722b486e8b1f431e93fd30848d6e5
Shivanah Shetty <shivanah.shetty@gmail.com>
2020-01-29 11:17:18 +0800

    starting e2e testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -12,4 +12,54 @@ describe('new App', () => {
     expect(page.getHeaderText()).toContain('ROBOTAB');
     expect(page.getCard()).toBeTruthy();
   });
+
+  it('Card should contain heading', () => {
+    page.navigateTo();
+    expect(page.getCardHeadingText()).toContain('Placement');
+  });
+
+  it('Card should contain row headings', () => {
+    page.navigateTo();
+    expect(page.getRowHeading1().getText()).toEqual('Column (X)');
+    expect(page.getRowHeading2().getText()).toEqual('Row (Y)');
+    expect(page.getRowHeading3().getText()).toEqual('Direction');
+  });
+
+  it('Card should contain Place Button', () => {
+    page.navigateTo();
+    expect(page.getPlaceButton().getText()).toEqual('PLACE');
+  });
+
+  it('Card should contain Move Button', () => {
+    page.navigateTo();
+    expect(page.getMoveButton().getText()).toEqual('MOVE');
+  });
+
+  it('Card should contain Left Button', () => {
+    page.navigateTo();
+    expect(page.getLeftButton().getText()).toEqual('LEFT');
+  });
+
+  it('Card should contain Right Button', () => {
+    page.navigateTo();
+    expect(page.getRightButton().getText()).toEqual('RIGHT');
+  });
+
+  it('Card should contain Report Button', () => {
+    page.navigateTo();
+    expect(page.getReportButton().getText()).toEqual('REPORT');
+  });
+
+  it('Report Button should add element to the list', () => {
+    page.navigateTo();
+    page.enterRowInput(2);
+    page.enterColInput(2);
+    page.enterDirInput('NORTH');
+    page.getPlaceButton().click();
+    page.getReportButton().click();
+    const itemslist = page.getList();
+    expect(itemslist.count()).toBe(1);
+    expect(itemslist.get(0).getText()).toBe('2,2,NORTH');
+  });
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -12,4 +12,57 @@ export class AppPage {
   getCard() {
     return element(by.deepCss('[class="sc-ion-card-md-h sc-ion-card-md-s md hydrated"]'));
   }
+
+  getCardHeadingText() {
+    return element(by.deepCss('app-home ion-card-title')).getText();
+  }
+
+  getRowHeading1() {
+    return element(by.deepCss('[id="ion-input-0-lbl"]'));
+  }
+
+  getRowHeading2() {
+    return element(by.deepCss('[id="ion-input-1-lbl"]'));
+  }
+
+  getRowHeading3() {
+    return element(by.deepCss('[id="ion-input-2-lbl"]'));
+  }
+
+  getPlaceButton() {
+    return element(by.deepCss('[id="place-btn"]'));
+  }
+
+  getMoveButton() {
+    return element(by.deepCss('[id="move-btn"]'));
+  }
+
+  getLeftButton() {
+    return element(by.deepCss('[id="left-btn"]'));
+  }
+
+  getRightButton() {
+    return element(by.deepCss('[id="right-btn"]'));
+  }
+
+  getReportButton() {
+    return element(by.deepCss('[id="report-btn"]'));
+  }
+
+  enterRowInput(text: number) {
+    return element(by.deepCss('[name="ion-input-0"]')).sendKeys(text);
+  }
+
+  enterColInput(text: number) {
+    return element(by.deepCss('[name="ion-input-1"]')).sendKeys(text);
+  }
+
+  enterDirInput(text: string) {
+    return element(by.deepCss('[name="ion-input-2"]')).sendKeys(text);
+  }
+
+  getList() {
+    return element.all(by.id('list-elements'));
+  }
+
 }
